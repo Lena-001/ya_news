ab74cf38223b9d9ace2d2f5653c871f3688a913f
Елена Губаева <el.gubaeva1909@gmail.com>
2023-10-01 10:31:51 +0300

    Не готово

---
+++ news/tests/test_routes.py
@@ -0,0 +1,41 @@
+# news/tests/test_routes.py
+# Импортируем класс HTTPStatus.
+from http import HTTPStatus
+
+from django.test import TestCase
+# Импортируем функцию reverse().
+from django.urls import reverse
+
+# Импортируем класс модели новостей.
+from news.models import News
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+
+    def test_pages_availability(self):
+        # Создаём набор тестовых данных - кортеж кортежей.
+        # Каждый вложенный кортеж содержит два элемента:
+        # имя пути и позиционные аргументы для функции reverse().
+        urls = (
+            # Путь для главной страницы не принимает
+            # никаких позиционных аргументов,
+            # поэтому вторым параметром ставим None.
+            ('news:home', None),
+            # Путь для страницы новости
+            # принимает в качестве позиционного аргумента
+            # id записи; передаём его в кортеже.
+            ('news:detail', (self.news.id,))
+        )
+        # Итерируемся по внешнему кортежуs
+        # и распаковываем содержимое вложенных кортежей:
+        for name, args in urls:
+            with self.subTest(name=name):
+                # Передаём имя и позиционный аргумент в reverse()
+                # и получаем адрес страницы для GET-запроса:
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
